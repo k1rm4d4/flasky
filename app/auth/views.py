d07c0079e43d8d64cf5e2440da80474c54071c83
Jaideep <jaideepsh25@gmail.com>
2024-04-14 13:51:36 +0530

    Add register route and view function.

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,9 @@ from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from . import auth 
 from ..models import User 
-from .forms import LoginForm
+from .. import db
+from .forms import LoginForm, RegistrationForm
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -18,10 +20,22 @@ def login():
         flash('Invalid username or password')    
     return render_template('auth/login.html', form=form)
 
-
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('You have been logged out!')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can login now!')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
