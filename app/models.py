dccb9426e20cd2560054fcdff8a1e8007faf6bae
Jaideep <jaideepsh25@gmail.com>
2024-05-12 17:11:30 +0530

    Add generate_confirmation_token, confirm methods to User model. Add imports

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer 
+from flask import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import UserMixin
@@ -21,6 +23,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -35,6 +38,23 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password=password)   
+
+    def generate_confirmation_token(self, expiration = 3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+    def confirm(self, token): 
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try: 
+            data = s.loads(token.encode('utf-8'))
+        except: 
+            return False 
+        if data.get('confirm') != self.id: 
+            return False 
+        self.confirmed = True
+        db.session.add(self)
+        return True 
+
 
 @login_manager.user_loader
 def load_user(user_id):
